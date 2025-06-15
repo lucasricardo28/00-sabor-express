a23afc3474b99e959e53b99eb52b71a39c3d5a52
Ricardo <lucasricardo28@gmail.com>
2025-06-14 22:34:18 -0300

    Adicionando anotação

--- Models/Restaurant.py
+++ Models/Restaurant.py
@@ -1,5 +1,7 @@
 class Restaurant:
-    
+    '''
+    A Classe restaurante é o meu primeiro model respresntando um restaurante.
+    '''
     restauranteList = []
 
     def __init__(self, name, category):
@@ -14,15 +16,24 @@ class Restaurant:
     
     @classmethod
     def list(cls):
+        '''
+        Método da classe para listar todos os restaurantes cadastrados!
+        '''
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurant in cls.restauranteList:
             print(f'{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} | {restaurant.active}')
 
     @property
     def active(self):
+        '''
+        Propriedade para conventer em texto.
+        '''
         return '✅' if self._is_active else '❌'
 
     def change_state(self):
+        '''
+        Método do objeto para mudar o status negando o antigo valor.
+        '''
         self._is_active = not self._is_active
 
 
